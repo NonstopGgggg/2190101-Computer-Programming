bc88d7e774da393dbf3f0324ea9b67625bf22a66
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-09-16 10:36:38 +0700

    Update code.py

--- Lecture05-ListProcessing/Grader05/★MissingDigits/code.py
+++ Lecture05-ListProcessing/Grader05/★MissingDigits/code.py
@@ -1,1 +1,17 @@
+string = input()
+# a list of integer to check if a digit exist
+digit = [0,0,0,0,0,0,0,0,0,0]
 
+for i in string:
+    if i in "1234567890":
+        digit[int(i)] = 1
+
+# use join to format the string in form 3,4,5
+answer = ",".join([str(i) for i in range(10) if digit[i] == 0])
+
+# If all digits are found, return none
+if not answer:
+    print("None")
+    
+else:
+    print(answer)
