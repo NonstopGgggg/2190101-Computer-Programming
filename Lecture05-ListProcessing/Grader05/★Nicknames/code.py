4f7151f47aa4340b294ab3be85465ad4056936ea
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-09-16 10:50:32 +0700

    Update code.py

--- Lecture05-ListProcessing/Grader05/★Nicknames/code.py
+++ Lecture05-ListProcessing/Grader05/★Nicknames/code.py
@@ -1,1 +1,22 @@
+real_names = ["Robert", "William", "James", "John", "Margaret", "Edward", "Sarah", "Andrew", "Anthony", "Deborah"]
+nicknames = ["Dick", "Bill", "Jim", "Jack", "Peggy", "Ed", "Sally", "Andy", "Tony", "Debbie"]
 
+n = int(input())
+names = []
+
+# Name input
+for i in range(n):
+    names.append(input())
+
+for name in names:
+    # Check if name exist in real_names
+    if name in real_names:
+        print(nicknames[real_names.index(name)])
+        
+    # check if name exist in nicknames
+    elif name in nicknames:
+        print(real_names[nicknames.index(name)])
+    
+    # For unknown names
+    else:
+        print("Not found")
