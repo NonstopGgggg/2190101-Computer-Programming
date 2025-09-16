9d4cde889ed90a1ba430c36bb5bfcbbab19c623f
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-09-16 11:26:51 +0700

    Update code.py

--- Lecture05-ListProcessing/Grader05/★Back_n_Front/code.py
+++ Lecture05-ListProcessing/Grader05/★Back_n_Front/code.py
@@ -1,1 +1,25 @@
+n = int(input())
+num = []
+
+# First set (red)
+for i in range(n):
+    num.append(int(input()))
+    
+# Second set (blue)
+num += [int(i) for i in input().split()]
+
+# Third set (purple)
+while True:
+    val = int(input())
+    
+    if val == -1:
+        break
+    
+    num.append(val)
+    
+# Display list by alternating front and back
+frontList = [num[i] for i in range(len(num)-1,-1,-2)]
+backList = [num[i] for i in range(len(num)-2,-1,-2)][::-1]
+
+print(frontList + backList)
 
