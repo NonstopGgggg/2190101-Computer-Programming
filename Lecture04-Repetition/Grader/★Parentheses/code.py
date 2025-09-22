99684cb745a1f78abb9448f3a051f81051f3d3e8
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-09-22 10:07:01 +0700

    Update code.py

--- Lecture04-Repetition/Grader/★Parentheses/code.py
+++ Lecture04-Repetition/Grader/★Parentheses/code.py
@@ -1,1 +1,19 @@
+string = input()
+
+target = ["(",")","[","]"]
+replace = ["[","]","(",")"]
+
+answer = ""
+for i in range(len(string)):
+    
+    if string[i] in "()[]":
+        
+        for j in range(4):
+            if string[i] == target[j]:
+                answer += replace[j]
+                
+    else:
+        answer += string[i]
+        
+print(answer)
 
