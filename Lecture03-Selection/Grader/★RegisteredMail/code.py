99c91b9e22f7bcf80a9d95cd6e2c33c2a10d1a9b
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-09-22 09:55:10 +0700

    Create code.py

---
+++ Lecture03-Selection/Grader/★RegisteredMail/code.py
@@ -0,0 +1,19 @@
+weight = int(input())
+
+if weight > 2000:
+    print("Reject")
+    
+elif weight < 101:
+    print(18)
+    
+elif weight < 251:
+    print(22)
+    
+elif weight < 501:
+    print(28)
+    
+elif weight < 1001:
+    print(38)
+    
+else:
+    print(58)
