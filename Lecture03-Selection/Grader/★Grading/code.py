0782cd9d6d759d5b5e4ca777cfcaa3a9828f9acb
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-09-22 09:39:48 +0700

    Create code.py

---
+++ Lecture03-Selection/Grader/★Grading/code.py
@@ -0,0 +1,16 @@
+score = float(input())
+
+if score < 50:
+    print("F")
+    
+elif score < 60:
+    print("D")
+    
+elif score < 70:
+    print("C")
+    
+elif score < 80:
+    print("B")
+    
+elif score >= 80:
+    print("A")
