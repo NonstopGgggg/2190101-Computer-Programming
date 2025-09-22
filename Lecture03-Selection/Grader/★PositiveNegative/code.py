5ac70f80b8ad07bde85783918746bebf755acef8
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-09-22 09:41:51 +0700

    Create code.py

---
+++ Lecture03-Selection/Grader/★PositiveNegative/code.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+if num < 0:
+    print("negative")
+    
+elif num == 0:
+    print("zero")
+    
+else:
+    print("positive")
+    
+if num % 2 == 0:
+    print("even")
+    
+else:
+    print("odd")
