5c64723299acf6cf1097c13ab9f43a0acb522545
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-10-14 09:30:22 +0700

    Update code.py

--- Lecture08-BasicDict/grader/★ReverseNKey/code.py
+++ Lecture08-BasicDict/grader/★ReverseNKey/code.py
@@ -1,1 +1,9 @@
+# We are to write two functions: reverse() and key()
+# Reverse(d) returns a dictionary where keys and values are swapped
+# Key(d,v) returns a list of key containing value "v"
 
+def reverse(d):
+    # dictionary has no repeated value
+    return {v:k for k, v in d.items()}
+    
+exec(input().strip())
