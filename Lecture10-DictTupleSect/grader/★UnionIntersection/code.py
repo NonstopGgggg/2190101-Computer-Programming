ea5445ed470db4f881fd8f174ef19ddc7b8f869a
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-11-04 09:39:22 +0700

    Update code.py

--- Lecture10-DictTupleSect/grader/★UnionIntersection/code.py
+++ Lecture10-DictTupleSect/grader/★UnionIntersection/code.py
@@ -1,1 +1,17 @@
+# given n sets, find the union and intersection of them.
+n = int(input())
+ans = [set(),set()]
 
+# find union and intersection
+for i in range(n):
+    val = set(input().split())
+    
+    if i != 0:
+        ans[1] = ans[1].intersection(val)
+        ans[0] = ans[0].union(val)
+        
+    else:
+        ans = [val,val]
+        
+print(len(ans[0]))
+print(len(ans[1]))
