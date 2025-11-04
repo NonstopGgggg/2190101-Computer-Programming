3ebfeda51c75d740b36e15853c1ee03aff156a2f
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-11-04 10:17:41 +0700

    Update code.py

--- Lecture10-DictTupleSect/grader/★Winner/code.py
+++ Lecture10-DictTupleSect/grader/★Winner/code.py
@@ -1,1 +1,32 @@
+# given n lines of two football team
+# the first team wins and the second team loses
+# find the team that never lose.
 
+# input
+n = int(input())
+winner = set([])
+loser = set([])
+
+# find winner with no losses
+for i in range(n):
+    win,lose = input().strip().split()
+    
+    # winner in loser does not count
+    if win in loser:
+        pass
+    
+    # winner without losses counts
+    elif win not in winner:
+        winner.add(win)
+        
+    # loser in winner must be removed
+    if lose in winner:
+        winner.remove(lose)
+        loser.add(lose)
+        
+    # loser not in loser is added
+    elif lose not in loser:
+        loser.add(lose)
+        
+# output
+print(sorted(winner))
