f2a6bfb55c17129b73f88efda7ce317156401db2
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-11-04 11:48:46 +0700

    Update code.py

--- Lecture10-DictTupleSect/grader/★★Cartoon/code.py
+++ Lecture10-DictTupleSect/grader/★★Cartoon/code.py
@@ -1,1 +1,25 @@
+# input
+animals = {}
 
+while True:
+    text = input().strip().split(",")
+    
+    if text[0] == "q":
+        break
+
+    name,animal = text[0], text[1]
+    animal = animal.strip()
+    
+    animals[animal] = animals.get(animal,[]) + [name]
+
+# output
+for live in animals:
+    print(f"{live}",end=": ")
+    
+    for i in range(len(animals[live])):
+        print(animals[live][i],end="")
+        
+        if i < len(animals[live])-1:
+            print(", ",end="")
+            
+    print("")
