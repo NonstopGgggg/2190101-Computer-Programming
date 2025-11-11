7cfe2dd7f701e2291cb6291ebabfc797119c20f2
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-11-11 10:01:31 +0700

    Update code.py

--- Lecture11-Numpy/Grader/★LogisticRegression/code.py
+++ Lecture11-Numpy/Grader/★LogisticRegression/code.py
@@ -1,1 +1,14 @@
+import numpy as np
+def p(x):
+# x is an array with size n x 2, containing the number of
+# problems solved (column 0), and GPA (column 1) of n students.
+    
+# Return an array with size n, containing the probability that each
+# student will pass the course, calculated with the formula above.
 
+# Using NumPy will allow you to write this function without
+# using loops. (The answer is no more than 3 lines).
+    logit = -3.98 + 0.1*x[::,0] + 0.5*x[::,1]
+    return 1 / (1 + np.exp(-logit))
+
+exec(input().strip()) # This line is required for Grader to work.
