743809f066a67f37503dc3558b2d636ab45f394a
Danai <129929698+NonstopGgggg@users.noreply.github.com>
2025-11-17 12:26:07 +0700

    Update code.py

--- Lecture11-Numpy/Grader/★★PeakIndexes/code.py
+++ Lecture11-Numpy/Grader/★★PeakIndexes/code.py
@@ -1,18 +1,22 @@
 import numpy as np
+
 def peak_indexes(x):
 # x is an array containing values
 # return an array listing indexes of “peaks”
-    peaks = np.where((x[1:-1] > x[:-2]) & (x[1:-1] > x[2:]))[0] + 1
-    return peaks
+  right = x[2:]
+  left = x[:-2]
+  mid = x[1:-1]
+  
+  index = (mid > left) & (mid > right)
+  
+  return np.arange(1,len(index)+1)[index]
     
 def main():
-    d = np.array([float(e) for e in input().split()])
-    pos = peak_indexes(np.array(d))
-    if len(pos) > 0:
-        print(", ".join([str(e) for e in pos]))
-    else:
-        print("No peaks ")
+  d = np.array([float(e) for e in input().split()])
+  pos = peak_indexes(np.array(d))
+  if len(pos) > 0:
+      print(", ".join([str(e) for e in pos]))
+  else:
+      print("No peaks ")
         
-    exec(input().strip()) # Don't remove this line
-
-
+exec(input().strip()) # Don't remove this line
